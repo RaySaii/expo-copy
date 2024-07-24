874c04ac73f6a2bede01bd2a481a9a64e2b6872c
LeiWang714 <l.wang@natincs.com>
2024-07-24 13:57:00 +0800

    add fav

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,15 +1,17 @@
-import { BlockingSetInitialColorMode } from '@expo/styleguide';
-import { Html, Head, Main, NextScript } from 'next/document';
+import { BlockingSetInitialColorMode } from "@expo/styleguide"
+import { Html, Head, Main, NextScript } from "next/document"
 
 export default function Document() {
   return (
     <Html className="a-test-class">
-      <Head />
-      <body className="bg-screen text-default min-h-dvh">
+      <Head>
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <body className="min-h-dvh bg-screen text-default">
         <BlockingSetInitialColorMode />
         <Main />
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }
